f0f243bcae9f03c2107c9d88497040a331f6d7d5
junho lee <junho1273@gmail.com>
2022-10-12 19:56:21 +0900

    test2

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Text('하이'),
-      ),
-      color: Colors.blue,
+        home: Scaffold(
+          appBar: AppBar(
+            actions:[
+              IconButton(visualDensity:VisualDensity(horizontal: -4,vertical: -4),onPressed: (){}, icon: Icon(Icons.search_rounded)),
+              IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
+              IconButton(onPressed: (){},icon: Icon(Icons.add_alert),)
+            ],
+            title: Text('하이'),
+            centerTitle: true,
+            elevation: 0.0
+          ),
+          body: Align(
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1)
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                    Container(
+                        child: Image.asset('assets/hapari.png',fit:BoxFit.fitHeight),
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1)
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1)
+                      ),
+                      child: Column(
+                        children: const [
+                          Text('제목',style: TextStyle(fontSize: 20)),
+                          Text('설명'),
+                          Text('으이')
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )],
+            )
+          )
+        )
     );
   }
 }
